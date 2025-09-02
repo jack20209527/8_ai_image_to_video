b523065b1152e7f992aec03c67d2036678a47516
jack20209527 <1924523696@qq.com>
2025-09-02 15:09:54 +0800

    4

--- js/my-login.js
+++ js/my-login.js
@@ -78,7 +78,92 @@ function hideLoading() {
 // 获取更多图片
 function get_more_images(callback) {
 
-  // showLoading ()
+  // 获取 JSON 对象
+  const localUser = LocalStorageUtil.getUserObject();
+  if (localUser) {
+      console.log('本地数据-昵称:', localUser.name);
+      console.log('本地数据-头像:', localUser.head_url);
+      console.log('本地数据-uid:', localUser.uid); 
+      console.log('本地数据-project_id:', localUser.project_id);
+      console.log('本地数据-积分:', localUser.jifen);
+      console.log('本地数据-token:', localUser.token);
+      console.log('本地数据-token_expire:', localUser.token_expire);
+  } else {
+      console.log('本地数据为空');
+  }
+
+  const jsonData = {
+    "opt": "10", // 10是获取视频列表
+    // "google_token": response.credential,
+    "uid": "12", // localUser.uid,
+    "project_id": "0", // GlobalConfig.project_id,
+    "product_id": GlobalConfig.product_id, // 默认产品ID
+    "device_info": "6789",
+    "page_number": "1",
+  };
+
+  const jsonString = JSON.stringify(jsonData);
+  console.log(jsonString);
+
+  // url = "http://fakeaddressgenerator.info:39600/go/lo/" + jsonString
+  // url = "http://localhost:39603/go/lo/" + jsonString
+  // url = GlobalConfig.url + '/go/v/get_more'
+  url = GlobalConfig.url + '/go/p/get_more'
+  console.log(url);
+
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      // 'Custom-Header': 'CustomValue' // 其他自定义头部
+    },
+    body: jsonString
+  })
+    .then(response => {
+      // 检查响应的状态码
+      if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText);
+      }
+      // 将响应解析为 JSON
+      return response.json();
+    })
+    .then(data => {
+      // 处理解析后的数据
+      console.log('post 请求成功:', data);
+      console.log("返回的错误码: ", data.code);
+      console.log("返回的总页数: ", data.base_ext0);
+      try {
+        // const parsedData = JSON.parse(data);
+        const parsedData = data;
+        const videos = parsedData.data; // 视频列表
+
+        if (videos.length > 0) {
+          console.log("有图片");
+          image0 = videos[0];
+          console.log("视频地址:", image0.video_url);
+          console.log("视频标题:", image0.video_title);
+          console.log("视频封面:", image0.ext0);
+        }
+
+        // 在数据处理完成后调用回调函数，并传入需要的数据
+        if (typeof callback === 'function') {
+          callback(videos, data.base_ext0);   // 视频列表和总页码
+        }
+
+      } catch (error) {
+        console.error('Error parsing JSON:', error);
+      }
+    })
+    .catch(error => {
+      // 处理请求错误
+      console.error('GET 请求失败:', error);
+    })
+    .finally(() => {
+      hideLoading(); // 隐藏加载指示器
+    });
+}
+
+function get_more_videos(callback) {
 
   // 获取 JSON 对象
   const localUser = LocalStorageUtil.getUserObject();
@@ -102,7 +187,6 @@ function get_more_images(callback) {
     "product_id": GlobalConfig.product_id, // 默认产品ID
     "device_info": "6789",
     "page_number": "1",
-    "my_token": localUser.token
   };
 
   const jsonString = JSON.stringify(jsonData);
@@ -140,43 +224,18 @@ function get_more_images(callback) {
         const videos = parsedData.data; // 视频列表
 
         if (videos.length > 0) {
-          console.log("有图片");
-          image0 = videos[0];
-          console.log("视频地址:", image0.video_url);
-          console.log("视频标题:", image0.video_title);
-          console.log("视频封面:", image0.ext0);
-
-          // // 显示上传的图片
-          // const id_image_original = document.getElementById('id_image_original');
-          // id_image_original.src = image0.image_original;
-          // id_image_original.classList.remove('hidden'); // 显示图片元素
-
-          // const id_image_generated = document.getElementById('id_image_generated');
-          // id_image_generated.src = image0.image_generated;
-          // id_image_generated.classList.remove('hidden'); // 显示图片元素
-
+          console.log("有视频，一共", videos.length, "个");
+          videos.forEach(video => {
+            console.log("视频地址:", video.video_url);
+            console.log("视频标题:", video.video_title);
+            console.log("视频封面:", video.ext0);
+          });
         }
 
         // 在数据处理完成后调用回调函数，并传入需要的数据
         if (typeof callback === 'function') {
           callback(videos, data.base_ext0);   // 视频列表和总页码
         }
-
-        // document.getElementById('user-name').innerText = `Hello, ${userData.name} 当前积分:${userData.jifen}`;
-
-
-        // 显示头像
-        // const avatarDiv = document.getElementById('user_header');
-        // avatarUrl = userData.head_url
-        // // avatarDiv.style.backgroundImage = `url('${avatarUrl}')`;
-
-        // if (avatarUrl) {
-        //   avatarDiv.style.backgroundImage = `url('${avatarUrl}')`;
-        //   avatarDiv.classList.remove('bg-gray-400');
-        // }
-
-        // 隐藏加载状态
-        // document.getElementById('loadingState').classList.add('hidden');
 
       } catch (error) {
         console.error('Error parsing JSON:', error);
